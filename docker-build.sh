5ad1d1b315680fc7a64449e24011823dab9c81cf
Matthew Reider <matthew.reider@dynatrace.com>
2020-06-03 17:51:47 +0200

    docker

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,5 @@
 docker login
 docker build -t eatk8s .
 docker tag eatk8s mreider/eatk8s:$1
-docker push mreider/eatk8s:$1
+docker tag eatk8s mreider/eatk8s:latest
+docker push mreider/eatk8s:latest
